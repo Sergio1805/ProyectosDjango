fc94b0f211c6647d32818ebf8ea560d02b942ffa
Sergio <tomy_18_05_2000@hotmail.com>
2021-01-13 06:35:59 -0500

    filtros personalizados

--- s07proyecto001/miapp/views.py
+++ s07proyecto001/miapp/views.py
@@ -1,32 +1,37 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
-layout = """
-    
 
-"""
 def index(request):
+    estudiantes = [
+        'Sánchez Cárdenas Sergio',
+        'Bendezu Santisteban Anthony',
+        'Castillo Tordoya Gerardo',
+        'Mansilla Menéndez Patrick'
+                  ]
    
-    return render(request, 'index.html')
-
+    return render(request, 'index.html', {
+    'inicio': 'Inicio',
+    'mensaje' : 'Proyecto web con Django (desde el view)',
+    'estudiantes':estudiantes
+    } )
 def saludo(request):
     
-    return render(request, 'saludo.html')
+    return render(request, 'saludo.html',{'saludo': 'Saludo',
+    'nombre_autor' : 'Sergio Sánchez (desde el view)'
+    } )
 
 def rango(request):
+    numeros=[]
     a =10
-    b=20
-    resultado = f"""
-                <h2> Números de [{a}] hasta [{b}] </h2>
-                Resultado : <br>
-                <ul>
-    """
+    a1=a
+    b =20
+    
     while a<=b:
-        resultado += f"<li> {a} </li>"
+        numeros.append(a)
         a+=1
-
-    resultado += "</ul>"
-    return HttpResponse(layout + resultado)   
+    
+    return render(request,'rango.html',{'rango':'Rango','a':a1,'b':b,'numeros':numeros})   
 
 
 def rango2(request, a=0 ,b=100):
@@ -45,4 +50,4 @@ def rango2(request, a=0 ,b=100):
         a+=1
 
     resultado += "</ul>"
-    return HttpResponse(layout + resultado)   
+    return HttpResponse(resultado)   
